fca1f30398ad6b4552e910c76e46079260ed0dd7
TeddyFrye <111472179+TeddyFrye@users.noreply.github.com>
2025-11-06 15:42:38 -0600

    Implements placeholder behavior CLI.
    Co-authored-by: Amar Thakor <115903355+amarthakor@users.noreply.github.com>
    Co-authored-by: Caleb Soong <121209059+caleb-soong@users.noreply.github.com>
    Co-authored-by: Nathan Johnson <63086420+narjoDev@users.noreply.github.com>

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -2,7 +2,38 @@ import { Command } from "commander";
 
 export const loadCommands = (program: Command) => {
   program
-    .command("hello")
-    .description("test commands by saying hello")
-    .action(() => console.log("hello there!"));
+    .command("create")
+    .description("Creates an empty S3 Bucket for static hosting")
+    .argument("<basename>", "Base name for bucket")
+    .action(async (basename) => {
+      const bucketName = `${basename}-${Date.now()}`;
+
+      console.log(bucketName);
+    });
+
+  program
+    .command("sync")
+    .description("Uploads local files to S3 Bucket")
+    .requiredOption(
+      "-b, --bucket <bucket-name>",
+      "The name of the S3 Bucket you're deploying to"
+    )
+    .requiredOption("-d, --directory <local-directory>")
+    .action(async (options) => {
+      const bucketName = `${options.bucket}`;
+      const localDirectory = `${options.directory}`;
+
+      console.log(`Bucket name is ${bucketName}`);
+      console.log(`The name of the directory is ${localDirectory}`);
+    });
+
+  program
+    .command("destroy")
+    .description("Empties and tears down S3 Bucket")
+    .argument("<bucketname>", "Full name of bucket to be destroyed")
+    .action(async (bucketname) => {
+      // LOOK INTO COMMANDER PROMPT TOOLS
+      const bucket = bucketname;
+      console.log(`${bucket} successfully destroyed`);
+    });
 };
