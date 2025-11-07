57b730b660c2c9aa70acf9ac09012574b0c4262d
TeddyFrye <111472179+TeddyFrye@users.noreply.github.com>
2025-11-07 09:53:07 -0600

    Adds emptyBucket method.

--- src/lib/aws/s3.ts
+++ src/lib/aws/s3.ts
@@ -1,8 +1,12 @@
 import {
   CreateBucketCommand,
   DeleteBucketCommand,
+  DeleteObjectCommand,
+  paginateListObjectsV2,
   S3Client,
 } from "@aws-sdk/client-s3";
+import path from "node:path";
+import fsPromises from "node:fs/promises";
 
 export default class S3ClientService {
   client: S3Client;
@@ -18,10 +22,25 @@ export default class S3ClientService {
     await this.client.send(new DeleteBucketCommand({ Bucket: bucketName }));
     console.log(`${bucketName} was destroyed`);
   }
-}
 
-/*
-From a particular command, such as create, the action handler will do client=new S3ClientService class instance.
-As part of that new instance, it will create a new connection. That object will have the properties of 
-different commands that we define. 
-*/
+  async emptyBucket(bucketName: string): Promise<void> {
+    const paginator = paginateListObjectsV2(
+      { client: this.client },
+      { Bucket: bucketName }
+    );
+
+    for await (const page of paginator) {
+      if (page.Contents) {
+        for (const obj of page.Contents) {
+          console.log(`Deleting ${obj.Key}`);
+          await this.client.send(
+            new DeleteObjectCommand({
+              Bucket: bucketName,
+              Key: obj.Key,
+            })
+          );
+        }
+      }
+    }
+  }
+}
