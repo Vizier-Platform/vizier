ceb7e4ce28b295ceba6747314938b9bb6b09801a
TeddyFrye <111472179+TeddyFrye@users.noreply.github.com>
2025-11-13 14:29:40 -0600

    Adds S3 stack deployment via CDK.
    Co-authored-by: Amar Thakor <115903355+amarthakor@users.noreply.github.com>
    Co-authored-by: Caleb Soong <121209059+caleb-soong@users.noreply.github.com>
    Co-authored-by: Nathan Johnson <63086420+narjoDev@users.noreply.github.com>

---
+++ src/lib/aws/cdk-s3.ts
@@ -0,0 +1,33 @@
+// Import required packages
+import { Toolkit } from "@aws-cdk/toolkit-lib";
+import { App, RemovalPolicy, Stack } from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+
+export async function deployS3Stack(baseName: string): Promise<void> {
+  const toolkit = new Toolkit();
+
+  const cloudAssemblySource = await toolkit.fromAssemblyBuilder(async () => {
+    const app = new App();
+    const stack = new Stack(app, "VizierStack2");
+
+    const bucket = new s3.Bucket(stack, baseName, {
+      versioned: true,
+      websiteIndexDocument: "index.html",
+      publicReadAccess: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS_ONLY,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    new s3deploy.BucketDeployment(stack, "DeployFiles", {
+      // "DeployFiles" is a descriptor ID
+      sources: [s3deploy.Source.asset("../../../test-site")], // path to your build
+      destinationBucket: bucket,
+    });
+
+    return app.synth();
+  });
+
+  await toolkit.deploy(cloudAssemblySource);
+}
