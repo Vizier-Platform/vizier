09be6c80a221944b6fa558fb0bb905c8710ea3fe
Amar <115903355+amarthakor@users.noreply.github.com>
2025-11-14 10:51:32 -0800

    Added Fargate functionality to ecs cluster
    Co-authored-by: Nathan Johnson <63086420+narjoDev@users.noreply.github.com>

--- src/lib/aws/cdk-app-server.ts
+++ src/lib/aws/cdk-app-server.ts
@@ -15,8 +15,34 @@ export async function deployAppServer(): Promise<void> {
     });
 
     // Bandaid solution, look into fixing 'exactOptionalPropertyTypes' error
-    const cluster = new ecs.Cluster(stack, "Cluster", {
+    const cluster = new ecs.Cluster(stack, "VizierFargateCluster", {
       vpc: vpc as ec2.IVpc,
+      enableFargateCapacityProviders: true,
+    });
+
+    const taskDefinition = new ecs.FargateTaskDefinition(
+      stack,
+      "VizierFargateClusterTask"
+    );
+
+    taskDefinition.addContainer("web", {
+      image: ecs.ContainerImage.fromRegistry("nginxdemos/hello"),
+    });
+
+    new ecs.FargateService(stack, "FargateService", {
+      cluster: cluster as ecs.ICluster,
+      taskDefinition,
+      minHealthyPercent: 100,
+      capacityProviderStrategies: [
+        {
+          capacityProvider: "FARGATE_SPOT",
+          weight: 2,
+        },
+        {
+          capacityProvider: "FARGATE",
+          weight: 1,
+        },
+      ],
     });
 
     return app.synth();
