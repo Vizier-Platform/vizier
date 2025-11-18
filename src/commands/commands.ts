f190f4d884753af9082b7578f17cfd2ea5995c54
narjoDev <63086420+narjoDev@users.noreply.github.com>
2025-11-18 15:40:24 -0800

    give command files more descriptive names

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,13 +1,13 @@
 import { Command } from "commander";
 
 import init from "./init.js";
-import deploy from "./deploy.js";
+import deployStack from "./deployStack.js";
 import destroyStack from "./destroyStack.js";
-import create from "./create.js";
-import sync from "./sync.js";
+import createBucket from "./createBucket.js";
+import syncBucket from "./syncBucket.js";
 import ghSync from "./gh-sync.js";
-import empty from "./empty.js";
-import destroy from "./destroy.js";
+import emptyBucket from "./emptyBucket.js";
+import destroyBucket from "./destroyBucket.js";
 
 export const loadCommands = (program: Command) => {
   // command names specified here so that they are
@@ -16,11 +16,13 @@ export const loadCommands = (program: Command) => {
   // "help" is provided by commander
 
   init.load(program, "init");
-  deploy.load(program, "deploy");
+  deployStack.load(program, "deploy");
   destroyStack.load(program, "destroyStack");
-  create.load(program, "create"); // create bucket
-  sync.load(program, "sync"); // sync bucket
-  ghSync.load(program, "gh-sync"); // clone repository and sync to bucket
-  empty.load(program, "empty"); // empty a bucket
-  destroy.load(program, "destroy"); // destroy bucket
+
+  // deprecated bucket commands
+  createBucket.load(program, "create");
+  syncBucket.load(program, "sync");
+  ghSync.load(program, "gh-sync");
+  emptyBucket.load(program, "empty");
+  destroyBucket.load(program, "destroy");
 };
