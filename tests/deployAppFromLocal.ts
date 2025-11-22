493c1d05f76613d2b86ddfd22ed7386a901472e8
narjoDev <63086420+narjoDev@users.noreply.github.com>
2025-11-22 12:37:21 -0800

    remove deprecated tests

--- tests/deployAppFromLocal.ts
+++
@@ -1,7 +0,0 @@
-import { deployAppServer } from "../src/lib/aws/cdk-app-server";
-
-deployAppServer({
-  isImageLocal: true,
-  imagePath: "/home/nathan/dev-launch/capstone/prep/vps/simple-express",
-  containerPort: 3000,
-});
