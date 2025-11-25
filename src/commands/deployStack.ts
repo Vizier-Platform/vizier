f4126fd2ca8a6b92552ca69ed23f5adb75e3452c
narjoDev <63086420+narjoDev@users.noreply.github.com>
2025-11-25 14:34:30 -0800

    extract deployStack action

--- src/commands/deployStack.ts
+++ src/commands/deployStack.ts
@@ -15,61 +15,63 @@ export function loadDeployCommand(program: Command, commandName: string) {
   program
     .command(commandName)
     .description("Deploy static site to CloudFormation Stack")
-    .action(async () => {
-      const config: Config = configSchema.parse(
-        readProperties(".vizier/config.json")
-      );
-      const { stackType } = config;
+    .action(deployStack);
+}
 
-      let workflowPath: string;
+export async function deployStack() {
+  const config: Config = configSchema.parse(
+    readProperties(".vizier/config.json")
+  );
+  const { stackType } = config;
 
-      switch (stackType) {
-        case "front": {
-          await deployFrontendFromConfig(config);
-          console.log(chalk.green(`Static site deployed.`));
-          workflowPath = "../templates/syncAssets.yml";
-          break;
-        }
-        case "front+back": {
-          await deployFrontendWithServerFromConfig(config);
-          console.log(chalk.green(`Frontend and backend deployed.`));
-          workflowPath = "../templates/syncAssetsBuildImage.yml";
-          break;
-        }
-        case "back": {
-          await deployServerFromConfig(config);
-          console.log(chalk.green(`Server deployed.`));
-          workflowPath = "../templates/buildImage.yml";
-          break;
-        }
-        case "back+db": {
-          await deployServerWithDatabaseFromConfig(config);
-          console.log(chalk.green(`Server with database deployed.`));
-          workflowPath = "../templates/buildImage.yml";
-          break;
-        }
-        case "front+back+db": {
-          await deployFrontendWithServerWithDatabaseFromConfig(config);
-          console.log(chalk.green(`Frontend, backend, and database deployed.`));
-          workflowPath = "../templates/syncAssetsBuildImage.yml";
-          break;
-        }
-        default: {
-          const unhandledType: never = stackType;
-          console.error(chalk.red(`Unhandled stack type ${unhandledType}`));
-          process.exit(1);
-        }
-      }
+  let workflowPath: string;
 
-      const workflowText = fs.readFileSync(
-        path.join(path.dirname(fileURLToPath(import.meta.url)), workflowPath),
-        "utf8"
-      );
+  switch (stackType) {
+    case "front": {
+      await deployFrontendFromConfig(config);
+      console.log(chalk.green(`Static site deployed.`));
+      workflowPath = "../templates/syncAssets.yml";
+      break;
+    }
+    case "front+back": {
+      await deployFrontendWithServerFromConfig(config);
+      console.log(chalk.green(`Frontend and backend deployed.`));
+      workflowPath = "../templates/syncAssetsBuildImage.yml";
+      break;
+    }
+    case "back": {
+      await deployServerFromConfig(config);
+      console.log(chalk.green(`Server deployed.`));
+      workflowPath = "../templates/buildImage.yml";
+      break;
+    }
+    case "back+db": {
+      await deployServerWithDatabaseFromConfig(config);
+      console.log(chalk.green(`Server with database deployed.`));
+      workflowPath = "../templates/buildImage.yml";
+      break;
+    }
+    case "front+back+db": {
+      await deployFrontendWithServerWithDatabaseFromConfig(config);
+      console.log(chalk.green(`Frontend, backend, and database deployed.`));
+      workflowPath = "../templates/syncAssetsBuildImage.yml";
+      break;
+    }
+    default: {
+      const unhandledType: never = stackType;
+      console.error(chalk.red(`Unhandled stack type ${unhandledType}`));
+      process.exit(1);
+    }
+  }
 
-      writeText(".github/workflows/vizier.yml", workflowText);
-      console.log(
-        "GitHub Actions workflow written to .github/workflows/vizier.yml"
-      );
-      console.log("Use git push to trigger the re-deployment workflow.");
-    });
+  const workflowText = fs.readFileSync(
+    path.join(path.dirname(fileURLToPath(import.meta.url)), workflowPath),
+    "utf8"
+  );
+
+  writeText(".github/workflows/vizier.yml", workflowText);
+  console.log(
+    "GitHub Actions workflow written to .github/workflows/vizier.yml"
+  );
+  console.log("Use git push to trigger the re-deployment workflow.");
 }
