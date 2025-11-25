98e8ca7b28531ddf757179905563da70fa37639b
TeddyFrye <111472179+TeddyFrye@users.noreply.github.com>
2025-11-25 14:31:33 -0600

    Removes comments

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -6,11 +6,6 @@ import { loadDestroyCommand } from "./destroyStack.js";
 import { loadRemoveVizierCommand } from "./removeVizier.js";
 
 export const loadCommands = (program: Command) => {
-  // command names specified here so that they are
-  // - all visible in one place
-  // - decoupled from behavior definitions
-  // "help" is provided by commander
-
   loadInitCommand(program, "init");
   loadDeployCommand(program, "deploy");
   loadDestroyCommand(program, "destroy");
