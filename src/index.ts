052e08d062241b23418912bcdac7d9c05ec11683
Caleb Soong <121209059+caleb-soong@users.noreply.github.com>
2025-12-16 16:37:20 -0600

    updated to 1.0.1 in package.json and index.ts
    Co-authored-by: Nathan Johnson <63086420+narjoDev@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { program } from "commander";
 import { loadCommands } from "./commands/commands.js";
 
 program
-  .version("1.0.0")
+  .version("1.0.1")
   .name("vizier")
   .description("an AWS deployment platform");
 loadCommands(program);
